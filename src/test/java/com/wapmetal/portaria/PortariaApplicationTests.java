326e9e0912d78b0e67f65ef7a6f9cd38b132f25e
msa0044 <msa820258400044@gmail.com>
2022-06-29 12:29:13 -0300

    upload

---
+++ src/test/java/com/wapmetal/portaria/PortariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wapmetal.portaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
